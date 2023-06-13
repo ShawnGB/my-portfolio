c66842f9f517f1a96445d5b8f8e3c9f5a52cd78a
Shawn Becker <shawn@yptokey.com>
2023-06-13 22:52:57 +0200

    addinf background image

--- src/components/Hero/HeroComponent.tsx
+++ src/components/Hero/HeroComponent.tsx
@@ -1,14 +1,19 @@
+import ContactComonent from '../Contact/ContactComonent';
 import Shawn from '/public/assets/images/IMG_5789.jpg';
 
 export default function HeroComponent() {
   return (
-    <div className='hero_container'>
-      <div className='hero_container--headline'>
-        <h1>Shawn Gordon Becker</h1>
-        <h2>Web & digital product development</h2>
+    <div className='hero-container'>
+      <div className='hero-container__details'>
+        <>
+          <h1>Shawn Gordon Becker</h1>
+          <h2>Web & digital product development</h2>
+        </>
+        <br />
+        <ContactComonent />
       </div>
 
-      <img src={Shawn} alt="it's me :)" />
+      {/* <img src={Shawn} alt="it's me :)" /> */}
     </div>
   );
 }
