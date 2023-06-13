f2f795536176d639380ea745d8b1027f60fc84a2
Shawn Becker <shawn@yptokey.com>
2023-06-13 15:19:28 +0200

    added sass, hero WIP and images

---
+++ src/components/Hero/HeroComponent.tsx
@@ -0,0 +1,12 @@
+export default function HeroComponent() {
+  return (
+    <div className='hero_container'>
+      <div className='hero_container--headline'>
+        <h1>Shawn Gordon Becker</h1>
+        <h2>Web & digital product development</h2>
+      </div>
+
+      <img src='/assets/images/IMG_5789.jpg' alt="it's me :)" />
+    </div>
+  );
+}
