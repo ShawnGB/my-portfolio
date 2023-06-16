1d48e37d72f32b1cd6de4fa77628b07709b433a1
Shawn Becker <shawn@yptokey.com>
2023-06-16 11:03:55 +0200

    added key to cv map

--- src/components/CV/CvComponent.tsx
+++ src/components/CV/CvComponent.tsx
@@ -6,8 +6,8 @@ export default function CvComponent() {
   return (
     <div className='flex flex-row gap-4 absolut top-2/4 overflow-x-scroll  h-1/4'>
       {data.working &&
-        data.working.map((experience: WorkingExperience) => (
-          <CvCardComponent experience={experience} />
+        data.working.map((experience: WorkingExperience, index) => (
+          <CvCardComponent experience={experience} key={index} />
         ))}
     </div>
   );
