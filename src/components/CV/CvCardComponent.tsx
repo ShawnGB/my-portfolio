948a1fbd8aec8976b2da9a8a1bf8228957890b52
Shawn Becker <shawn@yptokey.com>
2023-06-20 16:35:12 +0200

    updated cv cards

--- src/components/CV/CvCardComponent.tsx
+++ src/components/CV/CvCardComponent.tsx
@@ -12,17 +12,17 @@ export default function CvCardComponent({ experience }: IProps) {
   ).href;
 
   return (
-    <div className='bg-primary-200 w-1/3 h-3/4  mt-10 bg-opacity-30 shadow-lg rounded-lg flex flex-shrink-0 gap-4 align-middle'>
+    <div className='bg-primary-200 w-1/3 h-2/3 mt-10 bg-opacity-30 shadow-lg rounded-lg flex flex-shrink-0 gap-4 align-middle'>
       <div className='relative -top-8 -left-8 bg-white h-20 w-full flex align-middle rounded-lg shadow-md'>
         <img className='m-auto max-h-10 max-w-5/6' src={imgURL} alt='' />
       </div>
-      <div className=' flex flex-col justify-between leading-normal py-8'>
-        <div className='mb-8'>
+      <div className=' flex flex-col justify-between my-4'>
+        <div className='w-full'>
           <p className='text-sm text-gray-600 flex items-center gap-x-2 text-accent-400 font-medium'>
             <TimeSpanIcon />
             {experience.span}
           </p>
-          <div className='flex flex-col justify-items-start  my-3 w-3/4 '>
+          <div className='flex flex-col justify-items-start  my-3 w-full'>
             <div className='font-regular text-xl text-primary-700'>
               {experience.conmpany}
             </div>
