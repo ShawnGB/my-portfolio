c83762e774a16e3a6313ef59f65b914233266a73
Shawn Becker <shawn@yptokey.com>
2023-06-21 13:46:35 +0200

    added langueage files, dynamically loading vc card from locales

--- src/components/CV/CvComponent.tsx
+++ src/components/CV/CvComponent.tsx
@@ -1,16 +1,36 @@
-import data from '../../assets/cvdata.json';
+import { useTranslation } from 'react-i18next';
+
 import { WorkingExperience } from '../../Models';
 import CvCardComponent from './CvCardComponent';
+import { useEffect, useState } from 'react';
 
 export default function CvComponent() {
+  const { i18n } = useTranslation();
+
+  const [data, setData] = useState<any>();
+
+  const language = i18n.language;
+
+  useEffect(() => {
+    // Dynamically import the language file based on the selected language
+    import(`../../locales/${language}.json`)
+      .then((module) => {
+        setData(module.default);
+      })
+      .catch((error) => {
+        console.error(`Error while importing language file: ${error}`);
+      });
+  }, [language]);
+
+  if (!data) {
+    return null;
+  }
+
   return (
     <div className='h-2/4'>
-      <h2 className='my-4 mx-8 text-primary-500 font-regular text-xl'>
-        Working expoerince &&
-      </h2>
       <div className='flex flex-row gap-20 align-middle h-5/6 overflow-x-scroll '>
-        {data.working &&
-          data.working.map((experience: WorkingExperience, index) => (
+        {data &&
+          data.about.cv.map((experience: WorkingExperience, index: number) => (
             <CvCardComponent experience={experience} key={index} />
           ))}
       </div>
