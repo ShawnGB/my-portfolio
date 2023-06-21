4fdc9b713c85884597b6c41c7e1651ea096b4287
Shawn Becker <shawn@yptokey.com>
2023-06-21 19:50:48 +0200

    made cv component responsive

--- src/components/CV/CvCardComponent.tsx
+++ src/components/CV/CvCardComponent.tsx
@@ -12,18 +12,16 @@ export default function CvCardComponent({ experience, index }: IProps) {
     import.meta.url
   ).href;
 
-  console.log(index);
-
   return (
     <div
-      className={`bg-primary-200 w-11/12 md:w-6/12 mt-8 bg-opacity-30 shadow-lg rounded-lg flex flex-shrink-0 gap-4 align-middle ${
-        index % 2 === 0 ? 'md:mr-auto' : 'md:ml-auto'
+      className={`bg-primary-200 w-full sm:w-1/2 mt-8 bg-opacity-30 shadow-lg rounded-lg flex flex-shrink-0 gap-4 align-middle ${
+        index % 2 === 0 ? 'sm:mr-auto' : 'sm:ml-auto'
       }`}
     >
       <div className='relative -top-8 -left-8 bg-white h-20 w-full flex align-middle rounded-lg shadow-md'>
         <img className='m-auto max-h-10 max-w-5/6' src={imgURL} alt='' />
       </div>
-      <div className=' flex flex-col justify-between my-4'>
+      <div className='flex flex-col justify-between my-4'>
         <div className='w-full'>
           <p className='text-sm text-gray-600 flex items-center gap-x-2 text-accent-400 font-medium'>
             <TimeSpanIcon />
@@ -38,7 +36,7 @@ export default function CvCardComponent({ experience, index }: IProps) {
             </div>
           </div>
 
-          <p className='text-gray-500 w-3/4 text-base text-justify  text-primary-500 '>
+          <p className='text-gray-500 w-full sm:w-3/4 text-base text-justify  text-primary-500'>
             {experience.description}
           </p>
         </div>
