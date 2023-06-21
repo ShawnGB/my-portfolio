f9a41da62dd2d679fad6070b96ce8a1d587bc5a6
Shawn Becker <shawn@yptokey.com>
2023-06-21 19:02:19 +0200

    home comonent basics with bg image

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,11 +1,20 @@
 import { useTranslation } from 'react-i18next';
+import DecorativeBackground from '../../assets/svg/icons/DecorativeBackground';
 
 export default function HomeComponent() {
   const { t } = useTranslation();
   return (
-    <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('greeting', { name: 'John' })}</p>
+    <div className='flex flex-col items-center justify-center h-screen relative'>
+      <div className='absolute top-1/4 left-1/2 opacity-80'>
+        <DecorativeBackground />
+      </div>
+      <div className='relative mx-auto max-w-2xl'>
+        <h1 className='text-primary-900 font-bold text-6xl text-center text-right'>
+          Elevating <br /> Digital Experiences
+          <br />
+          with <br /> Sustainable Solutions
+        </h1>
+      </div>
     </div>
   );
 }
