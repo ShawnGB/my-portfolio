6d668a5d9585d9a96a394948ce34f64631ab2eb0
Shawn GB <god.shawn@gmail.com>
2023-06-23 17:25:17 +0200

    line spacing hero, css style sheet cleanup

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -6,13 +6,13 @@ export default function HomeComponent() {
   return (
     <div className='flex flex-col items-center justify-center h-5/6 w-full absolute overflow-hidden'>
       <div>
-        <h1 className=' border border-primary-50 text-primary-700 shadow-lg'>
+        <h1 className='tracking-tight leading-tight border border-primary-50 text-primary-700 shadow-lg'>
           <div className='font-bold text-4xl sm:text-6xl md:text-8xl text-right whitespace-pre p-8'>
             {t('home.hero')}
           </div>
         </h1>
 
-        <h2 className='mt-4 ml-8 text-accent-500 font-sans font-light text-xl sm:text-2xl md:text-3xl'>
+        <h2 className='tracking-tight leading-tight mt-4 ml-8 text-accent-500 font-sans font-light text-xl sm:text-2xl md:text-3xl'>
           web &amp; product development
         </h2>
       </div>
