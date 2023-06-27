711f5fc11285479873dacf6bfb96352f14c03332
Shawn GB <god.shawn@gmail.com>
2023-06-27 13:44:58 +0200

    added tailwind config for darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -40,8 +41,43 @@ export default {
         800: '#93181c',
         900: '#700e13',
       },
+      darkMode: {
+        // Dark mode color palette
+        background: '#1f2937',
+        white: '#ffffff',
+        primary: {
+          50: '#f2f3f4',
+          100: '#e6e8ea',
+          200: '#ccd1d5',
+          300: '#b2b9bf',
+          400: '#99a1a8',
+          500: '#808990',
+          600: '#67727a',
+          700: '#4d5a62',
+          800: '#334148',
+          900: '#19292e',
+        },
+        accent: {
+          50: '#fcecee',
+          100: '#f9d3d1',
+          200: '#f6b8b0',
+          300: '#f49d8f',
+          400: '#f2806e',
+          500: '#EF4B35',
+          600: '#d83f2e',
+          700: '#b63325',
+          800: '#93181c',
+          900: '#700e13',
+        },
+      },
     },
-    extend: {},
+    extend: {
+      extend: {
+        backgroundColor: ['dark', 'dark-hover'],
+        borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
+        textColor: ['dark', 'dark-hover', 'dark-active'],
+      },
+    },
   },
-  plugins: [],
+  plugins: ['tailwindcss-dark-mode'],
 };
